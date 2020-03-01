2230350fb741c0fcb6f0a6f6c369b296248f338b
Nilesh Londhe <nilesh@cloudgeni.us>
2020-03-01 11:40:07 -0800

    provider version constraints

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
 provider "google" {
   credentials = file("credentials.json")
-  version = "~> 2.5"
+  version = "~> 2.20"
   project = var.project_id
   region  = var.region
+}
+
+provider "null" {
+  version = "~> 2.1"
 }
